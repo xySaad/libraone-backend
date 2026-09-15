ae4983954237280eed302d55aaf8bec8629f475d
xySaad <saadrm.dev@gmail.com>
2026-09-15 11:10:54 +0100

    change callbackRedirectURL in config

--- config/generated/config.go
+++ config/generated/config.go
@@ -14,25 +14,25 @@ func mustLoadEnv(path string) map[string]string {
 }
 var env_ENV = mustLoadEnv(".env")
 type Config struct {
-	PROFILE_PASSWORD string
-	GRAPHQL_LOGIN string
-	GiteaClientSecret string
-	GiteaRedirectURL string
 	GRAPHQL_PASSWORD string
+	GiteaClientSecret string
 	GiteaClientID string
+	GiteaRedirectURL string
 	CallbackRedirectURL string
 	PROFILE_LOGIN string
+	PROFILE_PASSWORD string
+	GRAPHQL_LOGIN string
 }
 
 var config = Config{
+	GRAPHQL_PASSWORD: env_ENV["GRAPHQL_PASSWORD"],
+	GiteaClientSecret: env_ENV["GITEA_CLIENT_SECRET"],
+	GiteaClientID: "e5f4ddaa-2369-4ae3-b51a-bfac995b3626",
+	GiteaRedirectURL: "https://libraone.undo.it/api/oauth/gitea/callback",
+	CallbackRedirectURL: "https://libraone.undo.it/accounts/gitea?close=true",
+	PROFILE_LOGIN: env_ENV["PROFILE_LOGIN"],
 	PROFILE_PASSWORD: env_ENV["PROFILE_PASSWORD"],
 	GRAPHQL_LOGIN: env_ENV["GRAPHQL_LOGIN"],
-	GiteaClientSecret: env_ENV["GITEA_CLIENT_SECRET"],
-	GiteaRedirectURL: "https://libraone.undo.it/api/oauth/gitea/callback",
-	GRAPHQL_PASSWORD: env_ENV["GRAPHQL_PASSWORD"],
-	GiteaClientID: "e5f4ddaa-2369-4ae3-b51a-bfac995b3626",
-	CallbackRedirectURL: "https://libraone.undo.it/login/gitea?close=true",
-	PROFILE_LOGIN: env_ENV["PROFILE_LOGIN"],
 }
 
 func GetConfig() Config { return config }
